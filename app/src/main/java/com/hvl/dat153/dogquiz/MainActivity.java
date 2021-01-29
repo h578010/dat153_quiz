f178e5869765fae3c2371acad022c40339e3e50f
Synnøve Sørensen <578010@stud.hvl.no>
2021-01-29 13:52:39 +0100

    Fixed problem with main activity

--- app/src/main/java/com/hvl/dat153/dogquiz/MainActivity.java
+++ app/src/main/java/com/hvl/dat153/dogquiz/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textViewHighscore = findViewById(R.id.textViewScore);
+        textViewHighscore = findViewById(R.id.text_view_highscore);
         loadHighscore();
 
         Button buttonStart = findViewById(R.id.button_start);
